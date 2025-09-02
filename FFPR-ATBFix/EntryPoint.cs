2ca8d33857ca567ae6b479bf3b95a4ecb3831461
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2025-09-02 22:34:09 +0200

    update to BepInEx 6.0.0-pre.2

--- FFPR-ATBFix/EntryPoint.cs
+++ FFPR-ATBFix/EntryPoint.cs
@@ -1,13 +1,9 @@
 ﻿using BepInEx;
-using BepInEx.IL2CPP;
+using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using Il2CppInterop.Runtime.Injection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using UnhollowerRuntimeLib;
 using UnityEngine;
 
 namespace FFPR_ATBFix
